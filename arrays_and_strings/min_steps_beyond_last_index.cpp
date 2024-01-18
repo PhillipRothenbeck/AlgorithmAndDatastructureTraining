7d05eb58801637edcbcc7a159d9ef4b894f3d436
Phillip Rothenbeck <philliprothenbeck@gmail.com>
2024-01-18 23:22:47 +0100

    + comments

--- arrays_and_strings/min_steps_beyond_last_index.cpp
+++ arrays_and_strings/min_steps_beyond_last_index.cpp
@@ -7,8 +7,8 @@
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -28,85 +28,86 @@ using namespace std;
 // Input: < 4, 1, 2, 3, 0, 1, 0 >
 // Output: 0
 
-// lösen durchiterieren von hinten nach vorne 
+// lösen durchiterieren von hinten nach vorne
 
 void print_vector(const vector<size_t> &A) {
-  std::cout << "<";
-  for(size_t i = 0; i < A.size(); i++) {
-    std::cout << " " << A[i] << " ";
-  }
-  std::cout << ">" << std::endl;
+    std::cout << "<";
+    for (size_t i = 0; i < A.size(); i++) {
+        std::cout << " " << A[i] << " ";
+    }
+    std::cout << ">" << std::endl;
 }
 
 size_t min_steps_beyond_last_index(const vector<size_t> &A) {
-  if(A.size() == 0) {
-    return 0;
-  }
-  size_t furthest = 0;
-  size_t min_steps = 0;
-  for (size_t i = 0; i <= furthest && furthest < A.size(); ++i) {
-    size_t old_furthest = furthest;
-    furthest = max(furthest, i + A[i]);
-    if(old_furthest != furthest) {
-      min_steps++;
+    if (A.size() == 0) {
+        return 0;
     }
-  }
-  if (furthest < A.size()) {
-    return 0;
-  } else {
-    return min_steps;
-  }
+    size_t furthest = 0;
+    size_t min_steps = 0;
+    for (size_t i = 0; i <= furthest && furthest < A.size(); ++i) {
+        // look if furthest has surpassed the and
+        size_t old_furthest = furthest;
+        furthest = max(furthest, i + A[i]);  // look if current value moves i farther than furthest
+        if (old_furthest != furthest) {
+            min_steps++;  // increment min steps if furthest has changed
+        }
+    }
+    if (furthest < A.size()) {
+        return 0;
+    } else {
+        return min_steps;
+    }
 }
 /*************** end assignment ***************/
 
 int main() {
-  {
-    vector<size_t> v;
-    assert(min_steps_beyond_last_index(v) == 0);
-  }
-  {
-    vector<size_t> v = {1};
-    assert(min_steps_beyond_last_index(v) == 1);
-  }
-  {
-    vector<size_t> v = {0};
-    assert(min_steps_beyond_last_index(v) == 0);
-  }
-  {
-    vector<size_t> v = {3, 3, 1, 0, 2, 0, 1};
-    assert(min_steps_beyond_last_index(v) == 4);
-  }
-  {
-    vector<size_t> v = {3, 3, 1, 0, 2, 0, 0};
-    assert(min_steps_beyond_last_index(v) == 0);
-  }
-  {
-    vector<size_t> v = {3, 2, 0, 0, 2, 0, 1};
-    assert(min_steps_beyond_last_index(v) == 0);
-  }
-  {
-    vector<size_t> v = {2, 4, 1, 1, 0, 2, 3};
-    assert(min_steps_beyond_last_index(v) == 3);
-  }
-  {
-    vector<size_t> v = {2, 1, 1, 3, 0, 2, 0};
-    assert(min_steps_beyond_last_index(v) == 4);
-  }
-  {
-    vector<size_t> v = {4, 1, 2, 3, 0, 1, 0};
-    assert(min_steps_beyond_last_index(v) == 0);
-  }
-  {
-    vector<size_t> v = {4, 1, 2, 3, 0, 2, 0};
-    assert(min_steps_beyond_last_index(v) == 3);
-  }
-  {
-    vector<size_t> v = {1, 1, 5, 1, 1, 1, 1, 4, 1, 1, 1};
-    assert(min_steps_beyond_last_index(v) == 4);
-  }
-  {
-    vector<size_t> v = {3, 0, 0, 4, 1, 1, 1, 4, 1, 1, 1};
-    assert(min_steps_beyond_last_index(v) == 3);
-  }
-  cout << "all tests passed" << endl;
+    {
+        vector<size_t> v;
+        assert(min_steps_beyond_last_index(v) == 0);
+    }
+    {
+        vector<size_t> v = {1};
+        assert(min_steps_beyond_last_index(v) == 1);
+    }
+    {
+        vector<size_t> v = {0};
+        assert(min_steps_beyond_last_index(v) == 0);
+    }
+    {
+        vector<size_t> v = {3, 3, 1, 0, 2, 0, 1};
+        assert(min_steps_beyond_last_index(v) == 4);
+    }
+    {
+        vector<size_t> v = {3, 3, 1, 0, 2, 0, 0};
+        assert(min_steps_beyond_last_index(v) == 0);
+    }
+    {
+        vector<size_t> v = {3, 2, 0, 0, 2, 0, 1};
+        assert(min_steps_beyond_last_index(v) == 0);
+    }
+    {
+        vector<size_t> v = {2, 4, 1, 1, 0, 2, 3};
+        assert(min_steps_beyond_last_index(v) == 3);
+    }
+    {
+        vector<size_t> v = {2, 1, 1, 3, 0, 2, 0};
+        assert(min_steps_beyond_last_index(v) == 4);
+    }
+    {
+        vector<size_t> v = {4, 1, 2, 3, 0, 1, 0};
+        assert(min_steps_beyond_last_index(v) == 0);
+    }
+    {
+        vector<size_t> v = {4, 1, 2, 3, 0, 2, 0};
+        assert(min_steps_beyond_last_index(v) == 3);
+    }
+    {
+        vector<size_t> v = {1, 1, 5, 1, 1, 1, 1, 4, 1, 1, 1};
+        assert(min_steps_beyond_last_index(v) == 4);
+    }
+    {
+        vector<size_t> v = {3, 0, 0, 4, 1, 1, 1, 4, 1, 1, 1};
+        assert(min_steps_beyond_last_index(v) == 3);
+    }
+    cout << "all tests passed" << endl;
 }
